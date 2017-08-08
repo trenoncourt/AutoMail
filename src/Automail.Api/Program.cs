3ad2242134f3d490285294ede85706ea3c7a6dcf
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-08-08 09:30:38 +0200

    Change api to lightweight

--- src/Automail.Api/Program.cs
+++ src/Automail.Api/Program.cs
@@ -1,16 +1,43 @@
-﻿using System.IO;
+﻿using System;
+using System.IO;
+using Automail.Api.Extensions;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 
 namespace Automail.Api
 {
     public class Program
     {
-        public static void Main(string[] args)
+        public static void Main()
         {
+            IConfigurationRoot config = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables().Build();
+            AppSettings appSettings = config.Get<AppSettings>();
+            
             var host = new WebHostBuilder()
                 .UseKestrel()
                 .UseContentRoot(Directory.GetCurrentDirectory())
-                .UseStartup<Startup>()
+                .ConfigureLogging(loggerFactory =>
+                {
+                    if (Environment.GetEnvironmentVariable("ASPNETCORE_ENVIRONMENT") == "Development")
+                        loggerFactory.AddConsole();
+                })
+                .ConfigureServices(services =>
+                {
+                    services.AddRouting();
+                    if (appSettings.Cors.Enabled)
+                    {
+                        services.AddCors();
+                    }
+                })
+                .Configure(app =>
+                {
+                    app.ConfigureCors(appSettings);
+                })
                 .Build();
 
             host.Run();
