ce1ba4e0f0d7e1f9076f01833ae8a2a4043c7a0e
Thibaut RENONCOURT <trenoncourt@access-it.fr>
2017-08-08 08:39:58 +0200

    Add Cors management

---
+++ src/Automail.Api/AppSettings.cs
@@ -0,0 +1,18 @@
+﻿namespace Automail.Api
+{
+    public class AppSettings
+    {
+        public Cors Cors { get; set; }
+    }
+
+    public class Cors
+    {
+        public bool Enabled { get; set; }
+
+        public string Methods { get; set; }
+
+        public string Origins { get; set; }
+
+        public string Headers { get; set; }
+    }
+}
